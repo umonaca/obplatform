91ba2849fdcb6912bcb6b27ce7bc01f2693fa9af
Wei <wmu100@syr.edu>
2021-11-17 01:44:46 -0500

    feat: downloader

--- obplatform/connector.py
+++ obplatform/connector.py
@@ -1,9 +1,14 @@
 """APIs to query and download archive from the remote database"""
+from __future__ import annotations
+
+import time
 from typing import Any, Dict, List
 
 import requests
 
-ENDPOINT = "https://ashraeobdatabase.com/api/v1"
+# TODO
+ENDPOINT = "http://api.ashraeobdatabase.com:30380"
+# ENDPOINT = "https://ashraeobdatabase.com"
 
 
 class Connector:
@@ -22,11 +27,80 @@ class Connector:
         """
         List all behaviors available in the database
         """
-        response = self.session.get(self.endpoint + "/behaviors")
+        response = self.session.get(self.endpoint + "/api/v1/behaviors")
         return response.json()  # type: ignore
+
+    def _start_export_job(self, behavior_ids: List[str], studies: List[str]) -> str:
+        """
+        Inform server to start compressing data files for the given behaviors
+        """
+        response = self.session.post(
+            self.endpoint + "/api/v1/exports",
+            json={
+                "behaviors": behavior_ids,
+                "studies": studies,
+            },
+        )
+
+        response.raise_for_status()
+
+        return response.headers["location"]
+
+    def _poll_export_job(self, job_uri: str) -> requests.Response:
+        """
+        Poll the export job status
+        """
+        while True:
+            response = self.session.get(self.endpoint + job_uri, stream=True)
+            response.raise_for_status()
+            if response.status_code == 202:
+                response.close()
+                time.sleep(1)
+                continue
+            elif response.status_code == 200:
+                return response
+
+    def download_export_job(
+        self, filename: str, behavior_ids: List[int | str], studies: List[int | str]
+    ) -> None:
+        """
+        Download the data archive (ZIP) for the given behaviors
+        """
+        _behavior_ids: List[str] = list(map(str, behavior_ids))
+        # Because there is an unfixed bug on the server side, it only accepts
+        # strings as study ids.
+        _studies: List[str] = list(map(str, studies))
+        job_uri = self._start_export_job(_behavior_ids, _studies)
+        response = self._poll_export_job(job_uri)
+
+        with open(filename, "wb") as f:
+            for chunk in response.iter_content(chunk_size=None):
+                if chunk:
+                    f.write(chunk)
+
+        # TODO: refactor with httpx
+        # TODO: async connector
+
+        # with open(filename, "wb") as f:
+        #     with httpx.stream("GET", self.endpoint + job_uri) as response:
+        #         for data in response.iter_bytes():
+        #             f.write(data)
+
+        # print(response.headers)
+        # response.close()
+        # large_response = self.session.get(self.endpoint + job_uri, stream=False)
+        # with open(filename, "wb") as f:
+        #     f.write(large_response.content)
+
+        # TODO: python logger
+        # print("finished")
 
 
 if __name__ == "__main__":
     # Test code
     connector = Connector(ENDPOINT)
     print(connector.list_behaviors())
+
+    connector.download_export_job(
+        "data.zip", ["Appliance_Usage", "Occupancy"], ["22", "11", "2"]
+    )
--- tests/test_connector.py
+++ tests/test_connector.py
@@ -5,14 +5,64 @@ from obplatform.connector import ENDPOINT, Connector
 
 
 @responses.activate
-def test_connector():
+def test_list_behaviors():
     with open("tests/fixtures/behaviors.json") as sample_file:
         behaviors: list = json.load(sample_file)
         responses.add(
             responses.GET,
-            f"{ENDPOINT}/behaviors",
+            f"{ENDPOINT}/api/v1/behaviors",
             json=behaviors,
             status=404,
         )
         connector = Connector(ENDPOINT)
         assert connector.list_behaviors() == behaviors
+
+
+@responses.activate
+def test_start_export():
+    responses.add(
+        method=responses.POST,
+        url=f"{ENDPOINT}/api/v1/exports",
+        headers={"location": "/api/v1/exports/15270ec7-56bb-46aa-b2a7-ff58a781a048"},
+        status=202,
+    )
+    connector = Connector(ENDPOINT)
+    assert (
+        connector._start_export_job(
+            behavior_ids=["Appliance_Usage", "Occupancy"], studies=["22", "11"]
+        )
+        == "/api/v1/exports/15270ec7-56bb-46aa-b2a7-ff58a781a048"
+    )
+
+
+@responses.activate
+def test_poll_export_job():
+    """Currently the server side implementation of the API is bad
+    It returns HTTP 202 and empty content when processing the job, but returns
+    ZIP and HTTP 200 when it is finished. There should not be surprises in
+    Content-Type. I will fix it real soon
+    """
+    # Process for 2 seconds
+    wait_period = 2
+    for i in range(wait_period):
+        responses.add(
+            method=responses.GET,
+            url=f"{ENDPOINT}/api/v1/exports/15270ec7-56bb-46aa-b2a7-ff58a781a048",
+            # json={"status": "in_progress"},
+            status=202,
+        )
+
+    # Return final result
+    responses.add(
+        method=responses.GET,
+        url=f"{ENDPOINT}/api/v1/exports/15270ec7-56bb-46aa-b2a7-ff58a781a048",
+        json={"status": "COMPLETED"},
+        status=200,
+    )
+    connector = Connector(ENDPOINT)
+    assert (
+        connector._poll_export_job(
+            "/api/v1/exports/15270ec7-56bb-46aa-b2a7-ff58a781a048"
+        ).json()["status"]
+        == "COMPLETED"
+    )
